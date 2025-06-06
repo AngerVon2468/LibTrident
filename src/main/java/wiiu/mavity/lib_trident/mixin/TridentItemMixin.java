3ca6dde62535e9af72f7f8b6448c3718e6c2dc33
AngerVon2468 <thomaschad0909@gmail.com>
2025-06-06 11:40:28 +1000

    Updated Mixin to handle tridents with durability of 1 or lower

--- src/main/java/wiiu/mavity/lib_trident/mixin/TridentItemMixin.java
+++ src/main/java/wiiu/mavity/lib_trident/mixin/TridentItemMixin.java
@@ -12,8 +12,9 @@ public class TridentItemMixin {
 
 	/**
 	 * Based on the way Vanilla handles the ability to throw Tridents, if you make a custom Trident with infinite durability (value of 0),
-	 * then it will be unable to be thrown because of the first check in {@link TridentItem#use}. This Mixin modifies that behaviour so that if your trident does
-	 * have a maximum durability of 0, it can still be thrown without having to override the {@link TridentItem#use} method.
+	 * or a durability of 1, then it will be unable to be thrown because of the first check in {@link TridentItem#use}.
+	 * This Mixin modifies that behaviour so that if your trident does
+	 * have a maximum durability of 1 or less, it can still be thrown without having to override the {@link TridentItem#use} method.
 	 */
 	@WrapOperation(
 		method = "use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/TypedActionResult;",
@@ -24,8 +25,9 @@ public class TridentItemMixin {
 	)
 	private int allowInfiniteDurability(ItemStack instance, Operation<Integer> original) {
 		// The check is "itemStack.getDamage() >= itemStack.getMaxDamage() - 1"
-		// Therefore, if return a smaller value than -1 only if the max durability is 0, and call original otherwise, it should be compatible.
-		if (instance.getMaxDamage() == 0) return -2;
+		// Therefore, if return a smaller value than -1 (the smallest possible value it would allow) only if the max durability is 1 or less
+		// and call original otherwise, it should be compatible.
+		if (instance.getMaxDamage() <= 1) return -2;
 		else return original.call(instance);
 	}
 }
