99c498e71f505251b8b4f8e58f4fa50368ad28fe
AngerVon2468 <thomaschad0909@gmail.com>
2025-06-08 21:02:47 +1000

    Update buildscript & make registry conditional

--- src/main/java/wiiu/mavity/lib_trident/LibTrident.kt
+++ src/main/java/wiiu/mavity/lib_trident/LibTrident.kt
@@ -2,12 +2,13 @@ package wiiu.mavity.lib_trident
 
 import wiiu.mavity.lib_trident.tag.LibTridentTags
 
-import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.loader.api.FabricLoader
 
 import net.minecraft.item.TridentItem
-import net.minecraft.registry.*
 import net.minecraft.util.Identifier
+import net.minecraft.registry.*
 
 import org.slf4j.*
 
@@ -17,7 +18,7 @@ object LibTrident : ModInitializer {
 	override fun onInitialize() {
 		LOGGER.info("LibTrident initialising!")
 		LibTridentTags
-		Registry.register(Registries.ITEM, Identifier(MOD_ID, "test_trident"), object : TridentItem(FabricItemSettings()) {})
+		if (FabricLoader.getInstance().isDevelopmentEnvironment) Registry.register(Registries.ITEM, Identifier(MOD_ID, "test_trident"), object : TridentItem(FabricItemSettings()) {})
 	}
 
 	const val MOD_ID: String = "lib_trident"
